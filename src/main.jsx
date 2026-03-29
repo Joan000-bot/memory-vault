766fb4e177df844af293c5f9f4dbd5a74f1d86fc
Virael <jl0439053@gmail.com>
2026-03-29 10:23:30 +0800

    ✦ Memory Vault

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import MemoryVault from './MemoryVault.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MemoryVault />
+  </React.StrictMode>,
+)
